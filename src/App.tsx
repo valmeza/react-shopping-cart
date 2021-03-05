0d1dbb19af3084ceebfa02f474db9090a03dcbf1
Valeria Meza <valeriameza173@gmail.com>
2021-03-04 23:06:53 -0600

    add styling to item.styles.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,57 @@
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+
+// components
+import Item from './Item/Item';
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+
+// styles
+import { Wrapper } from './App.styles';
+
+// types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  // first await is for when we convert to json
+  // await in () is for the api call itself
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
+  
+  console.log(data);
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if(isLoading) return <LinearProgress />
+  if(error) return <div>Something went wrong...</div>
+
   return (
-    <div className="App">
-      Start
-    </div>
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart}/>
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
